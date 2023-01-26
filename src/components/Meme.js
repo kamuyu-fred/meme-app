efce476bf13d138ef08d7dcaa7cbcc8b118de1a6
John <johnjustice766@gmail.com>
2023-01-26 21:48:41 +0300

    initial meme.js commit

---
+++ src/components/Meme.js
@@ -0,0 +1,23 @@
+import React, {createRef} from 'react'
+import { exportComponentAsJPEG, exportComponentAsPNG } from 'react-component-export-image';
+function Meme({imageSrc, inputs}) {
+   const componentRef = createRef()
+   return (
+     <div className="meme__holder">
+       <div id="meme" className="meme" ref={componentRef}>
+         {<img className="meme__image" src={imageSrc} alt="meme" width="100" />}
+         <h2 className="top">{inputs.toptext}</h2>
+         <h2 className="bottom">{inputs.bottomtext}</h2>
+       </div>
+       <div className="meme__exporter">
+       <button className="btn" onClick={() => exportComponentAsJPEG(componentRef)}>
+               Save as JPEG
+       </button>
+       <button className="btn" onClick={() => exportComponentAsPNG(componentRef)}>
+               Save as PNG
+       </button>
+           </div>
+       </div>
+   )
+ }
+ export default Meme;
